583eea5ee5cacd2fb280567b39bdfb71c7ffa57d
Martichka9 <Martichka9@users.noreply.github.com>
2018-08-14 10:09:06 +0300

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,11 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void{
     firebase.initializeApp({
-      apiKey: "<apikey>",
-      authDomain: "<authdomain>.firebaseapp.com"
+      apiKey: "AIzaSyBylcd4JfhWe7Ml3yJiH13zZZz2WbNzcW0",
+      authDomain: "exam-app-bc38c.firebaseapp.com",
+      databaseURL: "https://exam-app-bc38c.firebaseio.com",
+      projectId: "exam-app-bc38c",
+      storageBucket: "exam-app-bc38c.appspot.com"
     })
   }
 }
