3397c50a4d3f3c10e7093bfa67d9ca666156f142
Martichka9 <Martichka9@users.noreply.github.com>
2018-08-18 20:17:53 +0300

    fixes

--- src/app/books/books.service.ts
+++ src/app/books/books.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs/operators";
 import { ABook } from "../models/book.model";
-import { Observable } from "../../../node_modules/rxjs";
+import { Observable, empty } from "../../../node_modules/rxjs";
 import { AngularFireDatabase, AngularFireList, AngularFireObject } from 'angularfire2/database';
 import { Router } from "../../../node_modules/@angular/router";
 import { Toast, ToastrService } from "../../../node_modules/ngx-toastr";
@@ -17,6 +17,7 @@ export class BooksService{
    aBookRef : AngularFireList<ABook> = null;
    abookPath : string = baseUrl;
    bookReview : ABook;
+   tempTitle : string = "";
  
   constructor(private db: AngularFireDatabase,private router : Router, private toastr : ToastrService) {
     this.aBookRef = db.list(this.dbPath);
@@ -32,13 +33,38 @@ export class BooksService{
 
   editABook(key: string, value: any): void {
       this.aBookRef.update(key, value)
-      .then(res => this.toastr.info("Book updated successfuly!"),
+      .then(res => {this.toastr.info("Book updated successfuly!");
+                    this.router.navigate(['/books']);},
             res => this.toastr.warning(res,"Warning!"))
       .catch(error => this.toastr.error(error, "Warning"));
   }
 
   createABook(newBook : ABook){
-    this.aBookRef.push(newBook).then(res => this.toastr.info("Book updated successfuly!"),
+    this.aBookRef.push(newBook).then(res => {
+      this.aBookRef.update(res['key'],{upvotes : 0});
+      this.toastr.info("Book created successfuly!");
+      this.router.navigate(['/books']);},
     res => this.toastr.warning(res,"Warning!"));
+  }
+  
+  deleteBook(id :string){
+    if (id !== null && id !== "" && id !== undefined){
+      this.aBookRef.snapshotChanges().pipe(
+        map(booksList =>
+          booksList.map(c => ({ id: c.payload.key, ...c.payload.val() }))
+        )
+      ).pipe(map(booksList => booksList.find(book => book.id === id))
+      ).subscribe(book => {
+        this.tempTitle = book['title'];
+      },err => this.toastr.error(err,"Error!"));
+      this.aBookRef.remove(id);
+      this.toastr.warning(this.tempTitle + " book is permanently deleted.", "Warning!");
+    }
+    else{
+      this.toastr.error("You are not permited to delete this record!, Error!");
+    }
+  }
+
+  like(id:string){
   }
 }
--- src/app/books/create-book/create-book.component.ts
+++ src/app/books/create-book/create-book.component.ts
@@ -21,7 +21,7 @@ export class CreateBookComponent implements OnInit {
       description: ["",[Validators.required]],
       genre: ["",[Validators.required]],
       imagePath: ["",[Validators.required]],
-      bestseller: [0,[Validators.required]]
+      bestseller: [false,[Validators.required]]
     });
   }
 
