30ec621e9f45f0988b2fd16d4734652a124f8fcd
Martichka9 <Martichka9@users.noreply.github.com>
2018-08-29 01:07:40 +0300

    click added

--- src/app/books/book/book.component.ts
+++ src/app/books/book/book.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ABook } from '../../models/book.model';
 import { BooksService } from '../books.service';
 import { AuthService } from '../../authentication/authentication/auth.service';
+import { Router } from '../../../../node_modules/@angular/router';
  
 @Component({
   selector: 'app-book',
@@ -12,9 +13,12 @@ export class BookComponent implements OnInit {
  
   @Input() book: ABook;
  
-  constructor(private bServ: BooksService, private authServ : AuthService) { }
+  constructor(private bServ: BooksService, private authServ : AuthService, private router : Router) { }
  
   ngOnInit() {
   }
-  
+
+  review(id :string){
+    this.router.navigate([`/books/review/${id}`]);
+  }
 }
